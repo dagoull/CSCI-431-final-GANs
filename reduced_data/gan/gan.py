3b59dc3e0c05049b31ff0eec687dc66de891b4d2
jrtechs <jeffery@jrtechs.net>
2020-04-18 14:49:58 -0400

    reduced data scripts

---
+++ reduced_data/gan/gan.py
@@ -0,0 +1,193 @@
+# base PyTorch code from https://github.com/eriklindernoren/PyTorch-GAN/
+# liscensed under the permissive MIT liscense
+#
+# @author: Jeffery Russell 4-11-2020
+#
+# Updates:  addition of tensorboard logging for loss functions
+#           update to data loading for additional testing
+#           additional logging for training
+
+
+import argparse
+import os
+import numpy as np
+import math
+
+import torchvision.transforms as transforms
+from torch.utils.tensorboard import SummaryWriter
+from torchvision.utils import save_image
+
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torch.autograd import Variable
+
+import torch.nn as nn
+import torch.nn.functional as F
+import torch
+
+os.makedirs("images", exist_ok=True)
+
+
+n_epochs = 200
+batch_size = 64
+lr = 0.0002
+b1 = 0.5
+b2 = 0.999
+n_cpu = 8
+latent_dim = 100
+img_size = 28
+channels = 1
+sample_interval = 400
+
+img_shape = (channels, img_size, img_size)
+
+cuda = True if torch.cuda.is_available() else False
+
+
+# generic generative neural network
+class Generator(nn.Module):
+    def __init__(self):
+        super(Generator, self).__init__()
+
+        def block(in_feat, out_feat, normalize=True):
+            layers = [nn.Linear(in_feat, out_feat)]
+            if normalize:
+                layers.append(nn.BatchNorm1d(out_feat, 0.8))
+            layers.append(nn.LeakyReLU(0.2, inplace=True))
+            return layers
+
+        self.model = nn.Sequential(
+            *block(latent_dim, 128, normalize=False),
+            *block(128, 256),
+            *block(256, 512),
+            *block(512, 1024),
+            nn.Linear(1024, int(np.prod(img_shape))),
+            nn.Tanh()
+        )
+
+    def forward(self, z):
+        img = self.model(z)
+        img = img.view(img.size(0), *img_shape)
+        return img
+
+# generic discrimative neural network
+class Discriminator(nn.Module):
+    def __init__(self):
+        super(Discriminator, self).__init__()
+
+        self.model = nn.Sequential(
+            nn.Linear(int(np.prod(img_shape)), 512),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Linear(512, 256),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Linear(256, 1),
+            nn.Sigmoid(),
+        )
+
+    def forward(self, img):
+        img_flat = img.view(img.size(0), -1)
+        validity = self.model(img_flat)
+
+        return validity
+
+
+# Loss function
+adversarial_loss = torch.nn.BCELoss()
+
+# Initialize generator and discriminator
+generator = Generator()
+discriminator = Discriminator()
+
+if cuda:
+    generator.cuda()
+    discriminator.cuda()
+    adversarial_loss.cuda()
+
+# Configure data loader
+os.makedirs("../data/mnist", exist_ok=True)
+dataloader = torch.utils.data.DataLoader(
+    datasets.MNIST(
+        "../../data/mnist",
+        train=False,
+        download=True,
+        transform=transforms.Compose(
+            [transforms.Resize(img_size), transforms.ToTensor(), transforms.Normalize([0.5], [0.5])]
+        ),
+    ),
+    batch_size=batch_size,
+    shuffle=True,
+)
+
+# Optimizers
+optimizer_G = torch.optim.Adam(generator.parameters(), lr=lr, betas=(b1, b2))
+optimizer_D = torch.optim.Adam(discriminator.parameters(), lr=lr, betas=(b1, b2))
+
+Tensor = torch.cuda.FloatTensor if cuda else torch.FloatTensor
+
+
+# Writer will output to ./runs/ directory by default
+writer = SummaryWriter()
+
+# loops through training epoch
+for epoch in range(n_epochs):
+
+    # chunks by batch
+    for i, (imgs, _) in enumerate(dataloader):
+
+        # Adversarial ground truths
+        valid = Variable(Tensor(imgs.size(0), 1).fill_(1.0), requires_grad=False)
+        fake = Variable(Tensor(imgs.size(0), 1).fill_(0.0), requires_grad=False)
+
+        # Configure input
+        real_imgs = Variable(imgs.type(Tensor))
+
+        
+        # training for generator
+        optimizer_G.zero_grad()
+
+        # Sample noise as generator input
+        z = Variable(Tensor(np.random.normal(0, 1, (imgs.shape[0], latent_dim))))
+
+        # Generate a batch of images
+        gen_imgs = generator(z)
+
+        # Loss measures generator's ability to fool the discriminator
+        g_loss = adversarial_loss(discriminator(gen_imgs), valid)
+
+        g_loss.backward()
+        optimizer_G.step()
+
+        # Training for discriminator
+        optimizer_D.zero_grad()
+
+        # Measure discriminator's ability to classify real from generated samples
+        real_loss = adversarial_loss(discriminator(real_imgs), valid)
+        fake_loss = adversarial_loss(discriminator(gen_imgs.detach()), fake)
+        d_loss = (real_loss + fake_loss) / 2
+
+        d_loss.backward()
+        optimizer_D.step()
+
+        # total batches ran
+        batches_done = epoch * len(dataloader) + i
+
+        print(
+            "[Epoch %d/%d] [Batch %d/%d] [Batches Done: %d] [D loss: %f] [G loss: %f]"
+            % (epoch, n_epochs, i, len(dataloader), batches_done, d_loss.item(), g_loss.item())
+        )
+
+
+        if batches_done % sample_interval == 0:
+            save_image(gen_imgs.data[:25], "images/%d.png" % batches_done, nrow=5, normalize=True)
+
+        # logs the loss of the two neural networks to tensorboard
+        writer.add_scalar('D_Loss', d_loss.item(), batches_done)
+        writer.add_scalar('G_Loss', g_loss.item(), batches_done)
+
+writer.close() # tensorboard logger
+
+
+# how to access tensorboard logs
+print("Run the command line:\n" \
+    "--> tensorboard --logdir=runs " \
+    "\nThen open http://0.0.0.0:6006/ into your web browser")
